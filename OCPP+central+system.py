7e233a45331ffc93f9cf4e22fb77f8132897a743
adh4702 <adh4702@uw.edu>
2024-02-13 16:19:31 -0800

    Created a folder containing files for Loading Firmware

--- OCPP+central+system.py
+++ OCPP+central+system.py
@@ -1,1 +1,8 @@
 # Charging Station Management System
+# implement a basic server and charge point 
+# interaction using the Open Charge Point Protocol (OCPP) 2.0.1
+
+import asyncio
+import logging
+import websockets
+from datetime import datetime
