5274b68a5bd248dcf9e5d631308112aadf7d3169
adh4702 <adh4702@uw.edu>
2024-02-19 14:10:05 -0800

    current status of programs

--- building-scenario-1.py
+++ building-scenario-1.py
@@ -1,1 +1,7 @@
 # Possible code solution for building scenario 1
+
+'''
+The building energy usage is acting unusual – it is much lower than normal for this time period and does not appear to be changing much.  
+Is there a data issue with the meter where it may be frozen or not reporting accurate data?  
+What happens if the meter is under reporting the actual usage of the building for a period?
+'''
